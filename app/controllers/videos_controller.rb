7762c19f75d47ca1d19d844ef125620b49c7d44c
Todd Williams <twilliamsark@gmail.com>
2015-10-30 11:02:54 -0500

    pre-populate common settings on new

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,74 @@
-class VideosController < InheritedResources::Base
+class VideosController < ApplicationController
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+
+  # GET /videos
+  # GET /videos.json
+  def index
+    @videos = Video.all
+  end
+
+  # GET /videos/1
+  # GET /videos/1.json
+  def show
+  end
+
+  # GET /videos/new
+  def new
+    @video = Video.new
+    @video.aiki_format = AikiFormat.find_by(name: "Tiado") rescue nil
+    @video.sensei = Sensei.find_by(name: "John Bollinger") rescue nil
+    @video.style = Style.find_by(name: "Tachi Waza") rescue nil
+  end
+
+  # GET /videos/1/edit
+  def edit
+  end
+
+  # POST /videos
+  # POST /videos.json
+  def create
+    @video = Video.new(video_params)
+
+    respond_to do |format|
+      if @video.save
+        format.html { redirect_to @video, notice: 'Video was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @video }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /videos/1
+  # PATCH/PUT /videos/1.json
+  def update
+    respond_to do |format|
+      if @video.update(video_params)
+        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @video.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /videos/1
+  # DELETE /videos/1.json
+  def destroy
+    @video.destroy
+    respond_to do |format|
+      format.html { redirect_to videos_url }
+      format.json { head :no_content }
+    end
+  end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_video
+      @video = Video.find(params[:id])
+    end
 
     def video_params
       params.require(:video).permit(:name, :waza_id, :aiki_format_id, :kata_id, :sensei_id, :style_id, :rank_id, :on_test, :youtube_id)
